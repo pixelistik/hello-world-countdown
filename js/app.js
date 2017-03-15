2dab8a885083b317db05047e4215b707b557af35
Pixelistik <code@pixelistik.de>
2017-03-15 23:38:49 +0100

    Extract template into component

--- js/app.js
+++ js/app.js
@@ -3,13 +3,19 @@
 var Vue = require("vue");
 var Timer = require("./timer.js")();
 
-var app = new Vue({
-    el: "#app",
-    data: new Timer(),
+Vue.component("weeks-plus-days", {
+    template: '<div class="weeks-days"> {{ weekDay.weeks }}+{{ weekDay.days }} </div>',
+    data: function () {
+        return new Timer();
+    },
     computed: {
         weekDay: function () {
             var projectedDateParam = this.extractDateFromUrl(window.location.hash);
             return this.fromDays(280 - this.daysUntilProjectedDate(projectedDateParam, Date.now()));
         }
     }
+});
+
+var app = new Vue({
+    el: '#app'
 });
