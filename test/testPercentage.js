b11aa5dfcfe36f9e0c5fb27e6ddf868409f511f0
Pixelistik <code@pixelistik.de>
2017-03-20 23:04:31 +0100

    Style fixes

--- test/testPercentage.js
+++ test/testPercentage.js
@@ -1,18 +1,20 @@
-var assert = require("chai").assert;
-var Vue = require("vue");
-var Percentage = require("../js/Percentage.js");
+'use strict';
 
-describe("Percentage component", function () {
+var assert = require('chai').assert;
+var Vue = require('vue');
+var Percentage = require('../js/Percentage.js');
+
+describe('Percentage component', function () {
     var Component = Vue.extend(Percentage);
 
-    it("should calculate 0% correctly", function () {
+    it('should calculate 0% correctly', function () {
         var component = new Component({
             propsData: {day: 0}
         });
         assert.equal(component.percent, 0);
     });
 
-    it("should calculate 100% correctly", function () {
+    it('should calculate 100% correctly', function () {
         var component = new Component({
             propsData: {day: 280}
         });
