56a39753dfc496bf7f0101e91e41b169e156aaee
Pixelistik <code@pixelistik.de>
2017-03-25 22:46:50 +0100

    Code style
    "static" is a reserved term.

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-var static = require('node-static');
+'use strict';
+
+var nodeStatic = require('node-static');
 
 //
 // Create a node-static server instance to serve the './public' folder
 //
-var file = new static.Server('.', { cache: 60 });
+var file = new nodeStatic.Server('.', { cache: 60 });
 var port = Number(process.env.PORT || 5000);
 
 require('http').createServer(function (request, response) {
