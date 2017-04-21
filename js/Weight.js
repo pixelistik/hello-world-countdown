e3cc1f54481118d2eca5d9155c546bd0c223560c
Pixelistik <code@pixelistik.de>
2017-04-21 19:32:24 +0200

    Separate units look nicer

--- js/Weight.js
+++ js/Weight.js
@@ -179,7 +179,7 @@ var interpolateValueFromDaySeries = function (normalizedDataPoints, day) {
 
 var SizeWeight = {
     props: ['day'],
-    template: '<div class="card col s12 m6"><div class="card-content weight">{{ weight }}g</div></div>',
+    template: '<div class="card col s12 m6"><div class="card-content weight">{{ weight }} g</div></div>',
     computed: {
         size: function () {
             var daySeries = dataPoints.map(function (dataPoint) {
