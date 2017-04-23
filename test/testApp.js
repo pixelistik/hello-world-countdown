75daf95333cc11ac242812d0674f4cf528d43bea
Pixelistik <code@pixelistik.de>
2017-04-23 19:01:42 +0200

    Test toggle on longtap

--- test/testApp.js
+++ test/testApp.js
@@ -1,10 +1,16 @@
 'use strict';
 
 var assert = require('chai').assert;
+var sinon = require('sinon');
 var Vue = require('vue');
 var appFactory = require('../js/App.js');
 
-var windowStub = {};
+var windowStub = {
+    setTimeout: function (handler) {
+        // Call immediately
+        handler();
+    }
+};
 var documentStub = {};
 
 var Component = Vue.extend(appFactory(windowStub, documentStub, 100, ''));
@@ -20,5 +26,12 @@ describe('App', function () {
         assert.equal(component.displaySettings, false);
         component.toggleSettings();
         assert.equal(component.displaySettings, true);
+    });
+
+    it('should toggle visibility on long tap', function () {
+        sinon.spy(component, 'toggleSettings');
+
+        component.touchStart();
+        assert(component.toggleSettings.calledOnce);
     });
 });
