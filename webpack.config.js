09f0f648e609101dbec12d2b7333ed6800bd63e4
Pixelistik <code@pixelistik.de>
2017-07-01 23:53:44 +0200

    In production, use smaller non-debug build of Vue

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,16 @@
+'use strict';
+
+var vuePath = function () {
+    if (process.env.NODE_ENV === 'production') {
+        return 'vue/dist/vue.min.js';
+    }
+    return 'vue/dist/vue.js';
+};
+
 module.exports = {
     resolve: {
-      alias: {
-        vue: 'vue/dist/vue.js'
-      }
+        alias: {
+            vue: vuePath()
+        }
     }
 };
