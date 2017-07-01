5d9062514ea2f40dae187ab27120c9330949440f
Pixelistik <code@pixelistik.de>
2017-07-01 22:32:23 +0200

    Minify custom Materialize CSS
    This brings the size from 106 KB to 78 KB.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,23 +2,27 @@
 
 module.exports = function (grunt) {
     require('load-grunt-tasks')(grunt); // npm install --save-dev load-grunt-tasks
-
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 	// Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         sass: {
-            options: {
-                sourceMap: true
-            },
             dist: {
                 files: {
                     'css/materialize-custom.css': 'css/materialize-custom.scss'
                 }
             }
+        },
+        cssmin: {
+            target: {
+                files: {
+                    'css/materialize-custom.min.css': 'css/materialize-custom.css'
+                }
+            },
+            options: {
+                sourceMap: true
+            }
         }
     });
-
-
-    grunt.registerTask('default', ['sass']);
-
+    grunt.registerTask('default', ['sass', 'cssmin']);
 };
